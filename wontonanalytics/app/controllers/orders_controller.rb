62963f3546c3f7861390502c2816e85ae5f3ab53
Cynthia Koo <cyn.koo@gmail.com>
2015-06-22 21:13:20 -0400

    Install HAML and convert ERB to HAML files // Add some basic stats to Orders page

--- wontonanalytics/app/controllers/orders_controller.rb
+++ wontonanalytics/app/controllers/orders_controller.rb
@@ -1,5 +1,14 @@
 class OrdersController < ApplicationController
   def index
+    @orders = Order.all
+    @orders_left_to_ship = Order.where(date_shipped: nil)
+    @total_revenue = Order.all.sum(:order_net).to_i
+
+    @average_revenue_per_order = @total_revenue / @orders.count
+
+    @total_order_items = OrderItem.all.sum(:quantity)
+    @average_items_per_order = @total_order_items.to_f / @orders.count
+    @total_buyers = Order.select(:username).distinct.count
   end
 
   def new

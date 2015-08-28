b71b25db3407ab487d6448d35e5c141f66bd7b39
Cynthia Koo <cyn.koo@gmail.com>
2015-08-27 20:08:11 -0400

    Add dimsum clothesline as item and separate cicf baby shirts into other category

--- wontonanalytics/app/controllers/products_controller.rb
+++ wontonanalytics/app/controllers/products_controller.rb
@@ -6,6 +6,8 @@ class ProductsController < ApplicationController
     @tshirts_sold_count = OrderItem.joins(:product).where(products: {product_type: "T-shirt"}).sum(:quantity)
     @stickers_sold_count = OrderItem.joins(:product).where(products: {product_type: "Stickers"}).sum(:quantity)
     @laptopstickers_sold_count = OrderItem.joins(:product).where(products: {product_type: "Laptop Sticker"}).sum(:quantity)
+    @baby_sold_count = OrderItem.joins(:product).where(products: {product_type: "Baby"}).sum(:quantity)
+    @party_sold_count = OrderItem.joins(:product).where(products: {product_type: "Party"}).sum(:quantity)
 
     @total_revenue = (Order.where(refund: nil).sum(:order_net) + Order.where.not(refund:nil).sum(:adjusted_net_order_amount)).round(2)
     @cards_revenue = OrderItem.joins(:product).where(products: {product_type: "Card"}).sum(:item_total).round(2)
@@ -13,6 +15,8 @@ class ProductsController < ApplicationController
     @tshirts_revenue = OrderItem.joins(:product).where(products: {product_type: "T-shirt"}).sum(:item_total).round(2)
     @stickers_revenue = OrderItem.joins(:product).where(products: {product_type: "Stickers"}).sum(:item_total).round(2)
     @laptopstickers_revenue = OrderItem.joins(:product).where(products: {product_type: "Laptop Sticker"}).sum(:item_total).round(2)
+    @baby_revenue = OrderItem.joins(:product).where(products: {product_type: "Baby"}).sum(:item_total).round(2)
+    @party_revenue = OrderItem.joins(:product).where(products: {product_type: "Party"}).sum(:item_total).round(2)
 
     @average_revenue = (@total_revenue / @order_items_count).round(2)
     @average_cards_revenue = (@cards_revenue / @cards_sold_count).round(2)
@@ -20,12 +24,16 @@ class ProductsController < ApplicationController
     @average_tshirts_revenue = (@tshirts_revenue / @tshirts_sold_count).round(2)
     @average_stickers_revenue = (@stickers_revenue / @stickers_sold_count).round(2)
     @average_laptopstickers_revenue = (@laptopstickers_revenue / @laptopstickers_sold_count).round(2)
+    @average_baby_revenue = (@baby_revenue / @baby_sold_count).round(2)
+    @average_party_revenue = (@party_revenue / @party_sold_count).round(2)
 
     @cards = Product.where(product_type: 'Card')
     @prints = Product.where(product_type: 'Print')
     @tshirts = Product.where(product_type: 'T-shirt')
     @stickers = Product.where(product_type: 'Stickers')
     @laptopstickers = Product.where(product_type: 'Laptop Sticker')
+    @babyitems = Product.where(product_type: 'Baby')
+    @partyitems = Product.where(product_type: 'Party')
   end
 
   def new

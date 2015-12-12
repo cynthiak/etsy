b697a2c0782933f496eaa0f30a7ad7f5a99e9aa9
Cynthia Koo <cyn.koo@gmail.com>
2015-12-11 23:38:28 -0500

    Etsy fee upload fixed

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -8,111 +8,65 @@ class Expense < ActiveRecord::Base
   	#date
   	date = nil
 
-  	# transactions
-  	transactions_count = 0
-  	transactions_amount = 0.0
-
-    # auto-renew sold
-    autorenews_count = 0
-  	autorenews_amount = 0.0
-
-    # listing
-    listings_count = 0
-    listings_amount = 0.0
-
-    # multi-quantity
-    multiquantity_count = 0
-    multiquantity_amount = 0.0
-
-    # promoted listings
-    promotedlistings_count = 0
-    promotedlistings_amount = 0.0
-
-    # private listing
-    privatelisting_count = 0
-    privatelisting_amount = 0.0
-
+    #hash
+    etsy_fee_counts = Hash.new
+    etsy_fee_amount = Hash.new
 
     CSV.foreach(file.path, headers:true) do |row|
       if date == nil
-      	date = DateTime.strptime(row[0], "%m/%d/%Y").strftime("%Y/%m/%d")
+        date = DateTime.strptime(row[0], "%m/%d/%Y").strftime("%Y/%m/%d")
       end
 
-      if row[3] == "transaction"
-      	transactions_count = transactions_count + 1
-      	transactions_amount = transactions_amount + row[5].to_f
-      elsif row[3] == "auto-renew sold "
-      	autorenews_count = autorenews_count + 1
-      	autorenews_amount = autorenews_amount + row[5].to_f
-      elsif row[3] == "listing"
-      	listings_count = listings_count + 1
-      	listings_amount = listings_amount + row[5].to_f
-      elsif row[3] == "multi-quantity"
-      	multiquantity_count = multiquantity_count + 1
-      	multiquantity_amount = multiquantity_amount + row[5].to_f
-      elsif row[3] == "Promoted Listings"
-      	promotedlistings_count = promotedlistings_count + 1
-      	promotedlistings_amount = promotedlistings_amount + row[5].to_f
-      elsif row[3] == "private listing"
-      	privatelisting_count = privatelisting_count + 1
-      	privatelisting_amount = privatelisting_amount + row[5].to_f
+      activity = row[3]
+      payment = row[4].to_f
+      fee = row[5].to_f
+
+      if !etsy_fee_counts.has_key?(activity)
+        etsy_fee_counts[activity] = 0
+        etsy_fee_amount[activity] = 0.0
+      end
+
+      etsy_fee_counts[activity] = etsy_fee_counts[activity] + 1
+
+      if (payment > 0 && activity.include?("credit"))
+        original_activity = activity.slice!(" credit")
+        if !etsy_fee_counts.has_key?(original_activity)
+          etsy_fee_amount[original_activity] = 0 - payment
+        else
+          etsy_fee_amount[original_activity] = etsy_fee_amount[original_activity] - payment
+        end
+        etsy_fee_amount[activity] = etsy_fee_amount[activity].to_f - payment
+      elsif (fee > 0)
+        etsy_fee_amount[activity] = etsy_fee_amount[activity] + fee
       end
 
     end
 
-    if transactions_count > 0
-  	  Expense.create({
-  	    :date => date,
-  	    :name => "Transaction Fees",
-  	    :description => transactions_count.to_s + " transactions",
-  	    :expense_type => "Etsy Fees",
-  	    :vendor => "Etsy",
-  	    :amount => transactions_amount.round(2)
-  	    })
-  	end
-
-  	if promotedlistings_count > 0
-  	  Expense.create({
-  	    :date => date,
-  	    :name => "Promoted Listings",
-  	    :description => promotedlistings_count.to_s + " promoted listings",
-  	    :expense_type => "Advertising",
-  	    :vendor => "Etsy",
-  	    :amount => promotedlistings_amount.round(2)
-  	    })
-  	end
-
-  	listings_amount_total = autorenews_amount + listings_amount + multiquantity_amount + privatelisting_amount
-  	if listings_amount_total > 0
-  	  listings_description = ""
-
-  	  ##### LISTINGS
-  	  if autorenews_count > 0
-          listings_description = listings_description + autorenews_count.to_s + " auto-renewals. "
-  	  end
-
-  	  if listings_count > 0
-  	    listings_description = listings_description + listings_count.to_s + " listings. "
-  	  end
-
-  	  if multiquantity_count > 0
-  	    listings_description = listings_description + multiquantity_count.to_s + " multiquantity listings. "
-  	  end
-
-  	  if privatelisting_count > 0
-  	    listings_description = listings_description + privatelisting_count.to_s + " private listings. "
-  	  end
-
-  	  Expense.create({
-  	    :date => date,
-  	    :name => "Listing Fees",
-  	    :description => listings_description,
-  	    :expense_type => "Etsy Fees",
-  	    :vendor => "Etsy",
-  	    :amount => listings_amount_total.round(2)
-  	    })
-  	end
+    ################### CREATE THE EXPENSE
+    etsy_fee_counts.each do |type, count|
+      if etsy_fee_amount[type] > 0
+        if type == "Promoted Listings"
+          Expense.create({
+            :date => date,
+            :name => "(Fees) " + count.to_s + " " + type,
+            :expense_type => "Advertising",
+            :vendor => "Etsy",
+            :amount => etsy_fee_amount[type].round(2)
+            })
+        else
+          Expense.create({
+            :date => date,
+            :name => "(Fees) " + count.to_s + " " + type,
+            :expense_type => "Etsy Fees",
+            :vendor => "Etsy",
+            :amount => etsy_fee_amount[type].round(2)
+            })
+        end
+      end
+    end
   end
+
+
 
 
 end

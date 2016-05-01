287bd2c4d0bd883e60f55581b1e23a2178792277
Cynthia Koo <cyn.koo@gmail.com>
2016-05-01 19:57:19 -0400

    Fix average monthly revenue function to accommodate zero retail or zero wholesale rev for a new month

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -27,8 +27,14 @@ module TimeHelper
       end_date = get_last_sale_date(order_type)
     end
     if order_type
-      start_date = Order.where(order_type: order_type, sale_date: start_date..end_date).order("sale_date").first.sale_date
-      end_date = Order.where(order_type: order_type, sale_date: start_date..end_date).order("sale_date").last.sale_date
+      orders = Order.where(order_type: order_type, sale_date: start_date..end_date).order("sale_date")
+      if !orders.empty?
+        start_date = orders.first.sale_date
+        end_date = orders.last.sale_date
+      else
+        start_date = get_first_sale_date(order_type)
+        end_date = get_last_sale_date(order_type)
+      end
     else
       start_date = Order.where(sale_date: start_date..end_date).order("sale_date").first.sale_date
       end_date = Order.where(sale_date: start_date..end_date).order("sale_date").last.sale_date
